578c8a75d51031f6e727fdb2659509439099642d
Sushil <sushil.maurya@gmail.com>
2016-04-07 21:15:55 -0500

    First Commit

---
+++ src/com/sushil/testprj1/TestMainClass.java
@@ -0,0 +1,11 @@
+package com.sushil.testprj1;
+
+public class TestMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
